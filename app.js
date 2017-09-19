afd24fae476a69973615925044fad25f45044f75
erika <samuels.erika@yahoo.com>
2017-09-19 12:03:52 -0400

    finished static routing

--- app.js
+++ app.js
@@ -4,28 +4,33 @@ const http = require('http');
 const PORT = 3000;
 const server = http.createServer();
 const nunjucks = require('nunjucks');
+const routes = require('./routes');         //the folder with the index.js file
+
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
-var obj = {
-    title: 'example',
-    people: [
-        {name: 'Josh'},
-        {name: 'Erika'}
-    ]
-}
+// var obj = {
+//     title: 'example',
+//     people: [
+//         {name: 'Josh'},
+//         {name: 'Erika'}
+//     ]
+// }
 
-nunjucks.render('index.html', obj, function (err, output) {
-    console.log(output);
-});
+// nunjucks.render('index.html', obj, function (err, output) {
+//     console.log(output);
+// });
 
 server.on('request', app);
 server.listen(PORT, function(){
     console.log('server listening')
 })
 
+app.use(express.static('public'))  //staticly routing to our public folder which contains our CSS stylesheets
+
+app.use('/', routes);   //all requests are sent to the routes subfolder (aka index.js)
 
 app.use(function(req, res, next){
     console.log('verb ', req.method, 'route ', req.path);
@@ -33,12 +38,12 @@ app.use(function(req, res, next){
 })
 
 app.get('/:stats', function(req, res, next){
-    if (req.params.stats == 'news'){
-        if (req.query.goodnews){
-            res.render( 'index', obj );
-        }
-        res.render( 'index', obj );
-    }
-    res.render( 'index', obj );
+    // if (req.params.stats == 'news'){
+    //     if (req.query.goodnews){
+    //         res.render( 'index', obj );
+    //     }
+    //     res.render( 'index', obj );
+    // }
+    // res.render( 'index', obj );
 })
 
