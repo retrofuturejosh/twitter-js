87a2a716dbda8ba8069e4fedd31730642d901496
Joshua Sohn <joshuadsohn@gmail.com>
2017-09-19 10:41:01 -0400

    adds npm start, goes through git collaboration in workshop

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ server.listen(PORT, function(){
     console.log('server listening')
 })
 
-app.get('/', function(req, res, next){
+app.use(function(req, res, next){
+    console.log('verb ', req.method, 'route ', req.path);
+    next();
+})
+
+app.get('/:stats', function(req, res, next){
+    if (req.params.stats == 'news'){
+        if (req.query.goodnews){
+            res.send('The news is good, right?');
+        }
+        res.send('The news is grim, right?');
+    }
     res.send('Hello World!');
 })
+
